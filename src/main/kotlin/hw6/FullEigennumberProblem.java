9be9e7476f7a363cc9cbcc1572c825242b4c541a
Niksen111 <nikiksen2003@gmail.com>
2024-05-07 16:41:22 +0300

    HW6: Added FullEigennumberProblem

---
+++ src/main/kotlin/hw6/FullEigennumberProblem.java
@@ -0,0 +1,133 @@
+package hw6;
+
+import kotlin.Pair;
+import org.apache.commons.lang.NotImplementedException;
+import org.jetbrains.annotations.NotNull;
+import org.la4j.Matrix;
+import org.la4j.Vector;
+import org.la4j.matrix.functor.MatrixAccumulator;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import static java.lang.Math.*;
+
+public class FullEigennumberProblem {
+    private FullEigennumberProblem() {}
+
+    @NotNull
+    public static Vector getDiagonal(@NotNull Matrix A) {
+        var vector = Vector.zero(A.rows());
+        for (int i = 0; i < A.rows(); i++) {
+            vector.set(i, A.get(i, i));
+        }
+
+        return vector;
+    }
+
+    @NotNull
+    public static Matrix getRotationMatrix(@NotNull Matrix A, int i, int j) {
+        double fi = 0.0;
+        if (abs(A.get(i, i) - A.get(j, j)) < 1e-15) {
+            fi = Math.PI / 4;
+        } else {
+            fi = Math.atan(-2 * A.get(i, j) / (A.get(i, i) - A.get(j, j))) / 2;
+        }
+
+        var cosFi = cos(fi);
+        var sinFi = sin(fi);
+        var result = Matrix.identity(A.rows());
+        result.set(i, i, cosFi);
+        result.set(j, j, cosFi);
+        result.set(i, j, -sinFi);
+        result.set(j, i, sinFi);
+
+        return result;
+    }
+
+    @NotNull
+    public static Pair<Vector, Integer> getEigennumbers(@NotNull Matrix matrix, double eps, ZeroingMethod method) {
+        final var n = matrix.rows();
+        var matrixAccumulator = new MatrixAccumulator() {
+            double sum = 0.0;
+            @Override
+            public void update(int i, int j, double value) {
+                if (i != j) {
+                    sum += value * value;
+                }
+            }
+
+            @Override
+            public double accumulate() {
+                return sum;
+            }
+        };
+        var A = matrix.copy();
+        var k = 0;
+
+        while (A.fold(matrixAccumulator) > eps) {
+            var coords = method.getZeroingIndexes(A);
+            var T = getRotationMatrix(A, coords.getFirst(), coords.getSecond());
+            A = T.transpose().multiply(A).multiply(T);
+            ++k;
+        }
+
+        return new Pair<>(getDiagonal(A), k);
+    }
+
+    @NotNull
+    public static List<Segment> getGershgorinCircles(@NotNull Matrix A) {
+        List<Segment> intervals = new ArrayList<>();
+        final var n = A.rows();
+        for (int i = 0; i < n; i++) {
+            final var row = A.getRow(i);
+            row.each((ind, value) -> abs(value));
+            final var radius = row.sum() - abs(A.get(i, i));
+            intervals.add(new Segment(A.get(i, i) - radius, A.get(i, i) + radius));
+        }
+
+        intervals.sort(Comparator.comparingDouble(Segment::getLeft));
+        List<Segment> merged = new ArrayList<>(List.of(intervals.get(0)));
+        for (Segment interval : intervals) {
+            var last = merged.getLast();
+            if (interval.getLeft() <= last.getRight()) {
+                last.setRight(max(last.getRight(), interval.getRight()));
+            } else {
+                merged.add(interval);
+            }
+        }
+
+        return merged;
+    }
+
+    interface ZeroingMethod {
+        public Pair<Integer, Integer> getZeroingIndexes(Matrix A);
+    }
+
+    static final class MaxElementMethod implements ZeroingMethod {
+        @Override
+        public Pair<Integer, Integer> getZeroingIndexes(@NotNull Matrix A) {
+            double maxMod = 0.0;
+            Pair<Integer, Integer> maxCoords = new Pair<>(0, 1);
+            for (int i = 0; i < A.rows(); i++) {
+                for (int j = 0; j < A.rows(); j++) {
+                    if (i != j && abs(A.get(i, j)) > maxMod) {
+                        maxMod = A.get(i, j);
+                        maxCoords = new Pair<>(i, j);
+                    }
+                }
+            }
+
+            return maxCoords;
+        }
+    }
+
+    static final class OptimalElementMethod implements ZeroingMethod {
+
+        @Override
+        public Pair<Integer, Integer> getZeroingIndexes(Matrix A) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/main/kotlin/hw6/Segment.java
@@ -0,0 +1,28 @@
+package hw6;
+
+public class Segment {
+    private double left;
+
+    private double right;
+
+    public Segment(double left, double right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public double getLeft() {
+        return left;
+    }
+
+    public void setLeft(double left) {
+        this.left = left;
+    }
+
+    public double getRight() {
+        return right;
+    }
+
+    public void setRight(double right) {
+        this.right = right;
+    }
+}
